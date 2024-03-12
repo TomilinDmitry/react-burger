eee8d92fd05df966b3edc3897acca01069fec601
Dmitry <dima.tomilin.95@mail.ru>
2024-03-12 17:45:36 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,15 @@ import { Provider } from 'react-redux';
 import { rootReducer } from './services/rootReducers';
 import { HashRouter } from 'react-router-dom';
 import { store } from './services/store';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
